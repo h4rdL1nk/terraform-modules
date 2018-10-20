f151d3f16777073315bc46d92c0c840196bf124d
h4rdl1nk <imthedanger@protonmail.com>
2018-10-20 16:51:47 +0200

    Added floating-ip module

---
+++ openstack/networking/floating-ip/init.tf
@@ -0,0 +1,11 @@
+resource "openstack_networking_floatingip_v2" "ips" {
+  count = "${var.number}"
+  pool  = "${var.ip-pool}"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "ips-assoc" {
+  count       = "${var.number}"
+  floating_ip = "${element(openstack_networking_floatingip_v2.ips.*.address,count.index)}"
+  instance_id = "${element(var.instance-ids,count.index)}"
+  fixed_ip    = "${element(var.instance-ips,count.index)}"
+}
---
+++ openstack/networking/floating-ip/vars.tf
@@ -0,0 +1,11 @@
+variable "number" {}
+
+variable "ip-pool" {}
+
+variable "instance-ids" {
+  type = "list"
+}
+
+variable "instance-ips" {
+  type = "list"
+}
