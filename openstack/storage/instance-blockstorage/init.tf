d62ce9d73fb6c16b6882645a3cd4173c2efdcaf7
h4rdl1nk <imthedanger@protonmail.com>
2018-10-20 16:14:11 +0200

    Added instance block storage module

---
+++ openstack/storage/instance-blockstorage/init.tf
@@ -0,0 +1,12 @@
+resource "openstack_blockstorage_volume_v2" "volume" {
+  count = "${var.number}"
+  name  = "${element(var.instance-names,count.index)}-swap"
+  size  = "${var.size}"
+}
+
+resource "openstack_compute_volume_attach_v2" "attach" {
+  count       = "${var.number}"
+  instance_id = "${element(var.instance-ids,count.index)}"
+  volume_id   = "${element(openstack_blockstorage_volume_v2.volume.*.id,count.index)}"
+  device      = "${var.device}"
+}
---
+++ openstack/storage/instance-blockstorage/vars.tf
@@ -0,0 +1,13 @@
+variable "number" {}
+
+variable "device" {}
+
+variable "size" {}
+
+variable "instance-names" {
+  type = "list"
+}
+
+variable "instance-ids" {
+  type = "list"
+}
