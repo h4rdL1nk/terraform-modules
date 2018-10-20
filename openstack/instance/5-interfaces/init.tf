b9b9b5b8b93939ea8a7eb86283fc3144ad521279
h4rdl1nk <imthedanger@protonmail.com>
2018-10-20 15:46:18 +0200

    Added instance

---
+++ openstack/instance/5-interfaces/init.tf
@@ -0,0 +1,24 @@
+resource "openstack_compute_instance_v2" "main" {
+  count           = "${var.number}"
+  name            = "${format("%s-%02d", var.name, count.index + 1)}"
+  image_name      = "${var.image}"
+  flavor_name     = "${var.flavor}"
+  key_pair        = "${var.keypair-name}"
+  security_groups = ["${var.security-group-names}"]
+
+  network { 
+      name = "${element(var.network-names,0)}"
+  }
+  network {
+      name = "${element(var.network-names,1)}" 
+  }
+  network {
+      name = "${element(var.network-names,2)}" 
+  }
+  network { 
+      name = "${element(var.network-names,3)}"
+  }
+  network {
+      name = "${element(var.network-names,4)}" 
+  }
+}
