f465d2ad64534ceae4d0c57783bebb3b408d761e
h4rdl1nk <imthedanger@protonmail.com>
2018-10-21 14:07:29 +0200

    Added volume devices output

--- openstack/storage/instance-blockstorage/output.tf
+++ openstack/storage/instance-blockstorage/output.tf
@@ -1,3 +1,7 @@
 output "volume-ids" {
   value = ["${openstack_compute_volume_attach_v2.attach.*.volume_id}"]
 }
+
+output "volume-devs" {
+  value = ["${openstack_compute_volume_attach_v2.attach.*.device}"]
+}
