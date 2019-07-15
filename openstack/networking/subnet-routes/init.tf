ebcaef1ca7582d050b3a9fcc2f863a9f089d9cf6
luismiguelsaez <luismiguelsaez@protonmail.com>
2019-07-15 23:54:40 +0200

    Fixed variable name

--- openstack/networking/subnet-routes/init.tf
+++ openstack/networking/subnet-routes/init.tf
@@ -1,6 +1,6 @@
 resource "openstack_networking_subnet_route_v2" "routes" {
   count            = "${length(var.subnet-ids)}"
-  subnet_id        = "${element(var.subnetn-ids,count.index)}"
+  subnet_id        = "${element(var.subnet-ids,count.index)}"
   destination_cidr = "${var.route-dest}"
   next_hop         = "${var.route-gw}"
 }
