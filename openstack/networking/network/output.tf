87dbf5cebc0cb73673248f358a03bcadb2b2099e
luismiguelsaez <luismiguelsaez@protonmail.com>
2019-10-24 18:26:38 +0200

    Added ID output for network module

--- openstack/networking/network/output.tf
+++ openstack/networking/network/output.tf
@@ -2,6 +2,10 @@ output "network-name" {
   value = "${openstack_networking_network_v2.networks.name}"
 }
 
+output "network-id" {
+  value = "${openstack_networking_network_v2.networks.id}"
+}
+
 output "subnet-ids" {
   value = "${openstack_networking_subnet_v2.subnets.*.id}"
 }
